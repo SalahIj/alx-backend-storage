4ec9a4f146fe19eb81cea3663883910f643ebdea
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-14 15:40:23 +0000

    tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""imported modules"""
+
+
+def top_students(mongo_collection):
+    """the function"""
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
