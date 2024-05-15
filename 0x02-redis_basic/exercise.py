8ae88e1aa875c40e391ac9a057cf2e48aad4e9e2
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-05-15 19:52:30 +0000

    Tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,87 @@
 #!/usr/bin/env python3
-"""Imported modules"""
+"""imported modules"""
 import redis
 from uuid import uuid4
 from functools import wraps
 from typing import Any, Callable, Optional, Union
 
 
+def count_calls(method: Callable) -> Callable:
+    """The function definition conut_call"""
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """Wraper function
+        Args:
+            args: the input
+            kwargs: the second input
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """The function definition call_history"""
+    @wraps(method)
+    def wrapper(self: Any, *args) -> str:
+        """Wrapper function
+        Args:
+            args: the input
+        """
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
+        output = method(self, *args)
+        self._redis.rpush(f'{method.__qualname__}:outputs', output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable) -> None:
+    """The replay function"""
+    client = redis.Redis()
+    calls = client.get(fn.__qualname__).decode('utf-8')
+    inputs = [input.decode('utf-8') for input in
+              client.lrange(f'{fn.__qualname__}:inputs', 0, -1)]
+    outputs = [output.decode('utf-8') for output in
+               client.lrange(f'{fn.__qualname__}:outputs', 0, -1)]
+    print(f'{fn.__qualname__} was called {calls} times:')
+    for input, output in zip(inputs, outputs):
+        print(f'{fn.__qualname__}(*{input}) -> {output}')
+
+
 class Cache:
-    """The class definition"""
+    """Class definition"""
     def __init__(self) -> None:
-        """The __init__ function"""
+        """__init__ method definition"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
-        """Store method"""
+        """Store method definition"""
         key = str(uuid4())
         client = self._redis
         client.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """Get method definition"""
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """Convert method definition"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """Convert bytes method definition"""
+        return int(data)
